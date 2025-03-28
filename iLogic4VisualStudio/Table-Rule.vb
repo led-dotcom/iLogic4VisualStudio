ea82ee4d7732be6ee39e205358003509752a74e4
led-dotcom <74643143+led-dotcom@users.noreply.github.com>
2025-03-27 21:18:58 -0400

    Create Table Rule

---
+++ iLogic4VisualStudio/Table-Rule.vb
@@ -0,0 +1,19 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Math
+Imports System.Windows.Forms
+Imports Autodesk.iLogic.Interfaces
+Imports Autodesk.iLogic.Runtime
+Imports Autodesk.iLogic.Types
+Imports Inventor
+
+Namespace iLogic4VisualStudio
+    Public Class Table_Rule
+        Inherits RuleBase
+
+        Public Overrides _
+        Sub Main()
+            MsgBox("Hello from Table Rule")
+        End Sub
+    End Class
+End Namespace
