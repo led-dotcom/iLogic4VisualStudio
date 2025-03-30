190ef0a36e5e4efc6c7804a17f41f24bc1d8d339
led-dotcom <74643143+led-dotcom@users.noreply.github.com>
2025-03-29 23:39:51 -0400

    Update MyTestRule.vb

--- iLogic4VisualStudio/MyTestRule.vb
+++ iLogic4VisualStudio/MyTestRule.vb
@@ -14,20 +14,27 @@ Namespace iLogic4VisualStudio
         Public Overrides _
         Sub Main()
             'Path to the Excel file
-            Dim excelPath As String = "Table.xlsx"
-            Dim sheetName As String = "Query1"
+            Dim excelPath As String = "Query.xlsx"
+            Dim sheetName As String = "Sheet1"
 
             'Assembly level params
             Dim choosedProject As String
-            Dim length As Double
-            Dim width As Double
+
+            'Part level params
+            Dim rack_width As Double
+            Dim leg_height As Double
 
             choosedProject = Parameter("Project")
-            GoExcel.FindRow(excelPath, sheetName, "Name", "=", choosedProject)
-            length = GoExcel.CurrentRowValue("Length")
-            width = GoExcel.CurrentRowValue("Width")
-            Parameter("LeftConectionDistance") = length
-            Parameter("RightConectionDistance") = width
+            GoExcel.FindRow(excelPath, sheetName, "Project", "=", choosedProject)
+            rack_width = GoExcel.CurrentRowValue("Rack Width")
+            leg_height = GoExcel.CurrentRowValue("Leg height")
+            Parameter("Undershelf - Copy:1", "Rack_Width") = rack_width
+            Parameter("TUBE - Copy:1", "Leg_height") = leg_height
+
+            '''Assembly level params, read only now
+            '''needs to update, be actual part params which showed in the form
+            Parameter("Rack_Width") = rack_width
+            Parameter("Leg_height") = leg_height
 
             'Renew the table immediately
             iLogicVb.UpdateWhenDone = True
