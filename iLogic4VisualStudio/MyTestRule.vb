a8869c0f336646db7662af3bfe91e827276c6725
led-dotcom <74643143+led-dotcom@users.noreply.github.com>
2025-04-02 00:27:59 -0400

    clean the code and add explanation

--- iLogic4VisualStudio/MyTestRule.vb
+++ iLogic4VisualStudio/MyTestRule.vb
@@ -14,63 +14,25 @@ Namespace iLogic4VisualStudio
         Public Overrides _
         Sub Main()
             Dim oDoc As DrawingDocument = ThisDoc.Document
-
-            'Dim oSheetOnly As Sheet = oDoc.ActiveSheet
-            'Dim oNote As DrawingNote
-            'oNote = oSheetOnly.DrawingNotes.Item(1)
-
-            'Dim oText As String = oNote.Text
-
-            'Logger.Info(oText)
-
-
-
-
             Dim oSheets As Sheets = oDoc.Sheets
 
-            For Each oSheet As Sheet In oSheets
-                Dim iNote As DrawingNote
-                For Each iNote In oSheet.DrawingNotes
-                    Logger.Info(iNote.Text)
+            ''' Loop through all sheets and all notes
+            ''' and print the note text to the logger
+            ''' i is the sheet number
+            ''' j is the note number
+            Dim i As Integer = 1
+            Dim j As Integer = 1
 
+            For Each oSheet As Sheet In oSheets
+                For Each iNote As DrawingNote In oSheet.DrawingNotes
+                    Logger.Info(i & ", " & j & "," & iNote.Text)
+
+                    j += 1
                 Next
 
-
+                j = 1
+                i += 1
             Next
-
-
-
-            'Dim oSheet1 As Sheet
-            'oSheet1 = oSheets.Item(2)
-
-            'Dim oNote1 As DrawingNote
-            'oNote1 = oSheet1.DrawingNotes.Item(0)
-
-
-            'Dim oModel As Document
-            'oModel = ThisDoc.ModelDocument
-
-            'Dim Result As DialogResult = MessageBox.Show("Turn all sketch symbol leaders on?", "Sketch symbol leaders toggle", MessageBoxButtons.YesNoCancel)
-            'Dim viewlabelresult As Boolean = False`
-            'If Result = DialogResult.Yes Then
-            '    viewlabelresult = True
-            'ElseIf Result = DialogResult.Cancel Then
-            '    viewlabelresult = False
-            'Else
-            '    Exit Sub
-            'End If
-
-
-
-            'Dim oSketchedSymbol As SketchedSymbol
-            'oSheets = oDoc.Sheets
-            'oSheet = oSheets.Item(1)
-            'For Each oSheet In oSheets
-            '    For Each oSketchedSymbol In oSheet.SketchedSymbols
-            '        oSketchedSymbol.LeaderVisible = True
-
-            '    Next
-            'Next
         End Sub
     End Class
 End Namespace
