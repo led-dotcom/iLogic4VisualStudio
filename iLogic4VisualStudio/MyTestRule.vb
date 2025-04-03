145eab7158f2aa9a41dc8b8f80f7ec76c6385fa5
led-dotcom <74643143+led-dotcom@users.noreply.github.com>
2025-04-02 22:35:55 -0400

    Update MyTestRule.vb

--- iLogic4VisualStudio/MyTestRule.vb
+++ iLogic4VisualStudio/MyTestRule.vb
@@ -13,6 +13,14 @@ Namespace iLogic4VisualStudio
 
         Public Overrides _
         Sub Main()
+            ' Reset all to #430
+
+            ' Check if this file is a drawing
+            If Not TypeOf (ThisApplication.ActiveDocument) Is DrawingDocument Then
+                MessageBox.Show("Drawing not active!")
+                Exit Sub
+            End If
+
             Dim oDoc As DrawingDocument = ThisDoc.Document
             Dim oSheets As Sheets = oDoc.Sheets
 
@@ -27,16 +35,26 @@ Namespace iLogic4VisualStudio
                 For Each iNote As DrawingNote In oSheet.DrawingNotes
                     Logger.Info(i & ", " & j & "," & iNote.Text)
 
-                    ''' Search for the text "GSS"
-                    ''' in the note text
+                    ' Search for the text "GSS" in the note text
                     Dim iText As String = iNote.Text
-                    Dim search As String = "GSS"
+                    Dim oText As String = ""
+                    Dim searchs() As String = {"20GSS", "20 GSS", "20G.S.S", "20 G.S.S", "20gss", "20 gss", "20g.s.s", "20 g.s.s"}
 
-                    If iText.Contains(search) Then
-                        Dim oText As String = Replace(iText, search, search & "!!!!")
+                    For Each search As String In searchs
+                        If iText.Contains(search) And iText.Contains("#304") Then
+                            oText = Replace(iText, "304", "430")
+                            Exit For
+                        ElseIf iText.Contains(search) And iText.Contains("304") Then
+                            oText = Replace(iText, "304", "#430")
+                            Exit For
+                        ElseIf iText.Contains(search) And Not iText.Contains("430") Then
+                            oText = Replace(iText, search, search & " #430")
+                            Exit For
+                        End If
+                    Next
 
-                        iNote.FormattedText = oText
-                    End If
+                    Logger.Info("change?: " & oText)
+                    If Not String.IsNullOrEmpty(oText) Then iNote.FormattedText = oText
 
                     j += 1
                 Next
@@ -44,6 +62,8 @@ Namespace iLogic4VisualStudio
                 j = 1
                 i += 1
             Next
+
+            MsgBox("All reset to #430")
         End Sub
     End Class
 End Namespace
