bd13512dbfe77fbe8959728398cd1b9f75f4c66b
led-dotcom <74643143+led-dotcom@users.noreply.github.com>
2025-04-02 22:52:55 -0400

    clean the test code

--- iLogic4VisualStudio/MyTestRule.vb
+++ iLogic4VisualStudio/MyTestRule.vb
@@ -16,29 +16,22 @@ Namespace iLogic4VisualStudio
             ' Reset all to #430
 
             ' Check if this file is a drawing
-            If Not TypeOf (ThisApplication.ActiveDocument) Is DrawingDocument Then
+            If TypeOf (ThisApplication.ActiveDocument) IsNot DrawingDocument Then
                 MessageBox.Show("Drawing not active!")
                 Exit Sub
             End If
 
+            Dim searchs() As String = {"20GSS", "20 GSS", "20G.S.S", "20 G.S.S", "20gss", "20 gss", "20g.s.s", "20 g.s.s"}
+
             Dim oDoc As DrawingDocument = ThisDoc.Document
             Dim oSheets As Sheets = oDoc.Sheets
 
-            ''' Loop through all sheets and all notes
-            ''' and print the note text to the logger
-            ''' i is the sheet number
-            ''' j is the note number
-            Dim i As Integer = 1
-            Dim j As Integer = 1
-
+            ''' Loop through all sheets and all notes and print the note text to the logger
             For Each oSheet As Sheet In oSheets
                 For Each iNote As DrawingNote In oSheet.DrawingNotes
-                    Logger.Info(i & ", " & j & "," & iNote.FormattedText)
 
-                    ' Search for the text "GSS" in the note text
                     Dim iText As String = iNote.FormattedText
                     Dim oText As String = ""
-                    Dim searchs() As String = {"20GSS", "20 GSS", "20G.S.S", "20 G.S.S", "20gss", "20 gss", "20g.s.s", "20 g.s.s"}
 
                     For Each search As String In searchs
                         If iText.Contains(search) And iText.Contains("#304") Then
@@ -53,14 +46,9 @@ Namespace iLogic4VisualStudio
                         End If
                     Next
 
-                    Logger.Info("change?: " & oText)
+                    'Logger.Info("change?: " & oText)
                     If Not String.IsNullOrEmpty(oText) Then iNote.FormattedText = oText
-
-                    j += 1
                 Next
-
-                j = 1
-                i += 1
             Next
 
             MsgBox("All reset to #430")
