4dded108de38fa6fbe5146ae44f10f96487bd884
led-dotcom <74643143+led-dotcom@users.noreply.github.com>
2025-07-17 09:32:46 -0400

    feat: Add height parameter to table generation loop
    Introduces a heightArray and iterates over height values when generating and exporting table images.

--- iLogic4VisualStudio/GenerateAndCompress.vb
+++ iLogic4VisualStudio/GenerateAndCompress.vb
@@ -17,57 +17,64 @@ Namespace iLogic4VisualStudio
         Public Overrides _
         Sub Main()
             Dim lengthArray As Integer() = {24, 36, 48}
-            Dim depthArray As Integer() = {24, 36}
+            Dim depthArray As Integer() = {24, 36, 48}
+            Dim heightArray As Integer() = {24, 36, 48}
 
             For Each ilength As Integer In lengthArray
                 For Each idepth As Integer In depthArray
-                    ' Change variable values of the parameters
-                    Parameter("Top:1", "d2") = ilength
-                    Parameter("Undershelf:1", "d1") = ilength - 4
+                    For Each iheight As Integer In heightArray
+                        ' Change variable values of the parameters
+                        Parameter("Top:1", "d2") = ilength
+                        Parameter("Undershelf:1", "d1") = ilength - 4
+                        Parameter("Channel_H:2", "d21") = ilength - 8 - 2 * 0.0625 - 0.0625
+                        Parameter("Channel_U:1", "d1") = ilength - 4.5
 
-                    Parameter("Top:1", "d1") = idepth
-                    Parameter("Undershelf:1", "d0") = idepth - 4
+                        Parameter("Top:1", "d1") = idepth
+                        Parameter("Undershelf:1", "d0") = idepth - 4
+                        Parameter("Channel_V:1", "d46") = idepth - 0.625
 
-                    'Renew the table immediately
-                    InventorVb.DocumentUpdate()
+                        Parameter("Leg:1", "d1") = iheight - 3
 
-                    Dim m_Doc As Inventor.Document = ThisDoc.Document
+                        'Renew the table immediately
+                        InventorVb.DocumentUpdate()
 
-                    Dim m_Camera As Inventor.Camera = ThisApplication.ActiveView.Camera
-                    Dim m_TO As Inventor.TransientObjects = ThisApplication.TransientObjects
+                        Dim m_Doc As Inventor.Document = ThisDoc.Document
 
-                    'm_Camera.Perspective = True
+                        Dim m_Camera As Inventor.Camera = ThisApplication.ActiveView.Camera
+                        Dim m_TO As Inventor.TransientObjects = ThisApplication.TransientObjects
 
-                    m_Camera.ViewOrientationType = Inventor.ViewOrientationTypeEnum.kIsoTopLeftViewOrientation
-                    m_Camera.Fit()
-                    m_Camera.ApplyWithoutTransition()
+                        'm_Camera.Perspective = True
 
-                    Dim m_CV As Inventor.View = ThisApplication.ActiveView
+                        m_Camera.ViewOrientationType = Inventor.ViewOrientationTypeEnum.kIsoTopLeftViewOrientation
+                        m_Camera.Fit()
+                        m_Camera.ApplyWithoutTransition()
 
-                    m_CV.DisplayMode = Inventor.DisplayModeEnum.kShadedRendering
-                    ThisApplication.DisplayOptions.Show3DIndicator = False
+                        Dim m_CV As Inventor.View = ThisApplication.ActiveView
 
-                    m_CV.Update()
+                        m_CV.DisplayMode = Inventor.DisplayModeEnum.kShadedRendering
+                        ThisApplication.DisplayOptions.Show3DIndicator = False
 
-                    ' Create name string for the saved image
-                    Dim code As String = "WT"
-                    Dim height As Int16 = Parameter("High")
+                        m_CV.Update()
 
-                    Dim saveName As String = code & "_" & ilength & "_" & idepth & "_" & height
-                    Dim exportPath As String = "C:\Users\di\Desktop\Export\"
-                    Dim tempImagePath As String = System.IO.Path.Combine(exportPath, saveName & "_temp.png")
-                    Dim finalImagePath As String = System.IO.Path.Combine(exportPath, saveName & ".jpg")
+                        ' Create name string for the saved image
+                        Dim code As String = "WT"
 
-                    ' First save the image as temporary PNG
-                    m_Camera.SaveAsBitmap(tempImagePath, 1024, 768)
+                        Dim saveName As String = code & "_" & ilength & "_" & idepth & "_" & iheight
+                        Dim exportPath As String = "C:\Users\di\Desktop\Export\"
+                        Dim tempImagePath As String = System.IO.Path.Combine(exportPath, saveName & "_temp.png")
+                        Dim finalImagePath As String = System.IO.Path.Combine(exportPath, saveName & ".jpg")
 
-                    ' Compress and convert to JPEG
-                    CompressAndSaveImage(tempImagePath, finalImagePath, 75) ' 75% quality
+                        ' First save the image as temporary PNG
+                        m_Camera.SaveAsBitmap(tempImagePath, 1024, 768)
 
-                    ' Clean up temporary file
-                    If System.IO.File.Exists(tempImagePath) Then
-                        System.IO.File.Delete(tempImagePath)
-                    End If
+                        ' Compress and convert to JPEG
+                        CompressAndSaveImage(tempImagePath, finalImagePath, 75) ' 75% quality
+
+                        ' Clean up temporary file
+                        If System.IO.File.Exists(tempImagePath) Then
+                            System.IO.File.Delete(tempImagePath)
+                        End If
+                    Next
                 Next
             Next
         End Sub
