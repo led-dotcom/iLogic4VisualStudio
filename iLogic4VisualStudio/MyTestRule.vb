ef4a64fae6a04eccbebd9a3462a4620e90c58e14
led-dotcom <74643143+led-dotcom@users.noreply.github.com>
2025-10-20 09:38:19 -0400

    feat: rename the parts

--- iLogic4VisualStudio/MyTestRule.vb
+++ iLogic4VisualStudio/MyTestRule.vb
@@ -23,9 +23,14 @@ Namespace iLogic4VisualStudio
             Dim oRefDocs As DocumentsEnumerator = oAsmDoc.AllReferencedDocuments
 
             ' Iterate through the list of documents.
-            Dim oRefDoc As Document
+            Dim oRefDoc As PartDocument
             For Each oRefDoc In oRefDocs
                 Logger.Info(oRefDoc.FullFileName)
+
+                Dim newName As String = System.IO.Path.GetFileNameWithoutExtension(oRefDoc.FullFileName) & "_Copy" & System.IO.Path.GetExtension(oRefDoc.FullFileName)
+                Logger.Info(newName)
+                ' Copy the document to a new file in the same folder.
+                oRefDoc.SaveAs(System.IO.Path.Combine(System.IO.Path.GetDirectoryName(oRefDoc.FullFileName), newName), False)
             Next
         End Sub
     End Class
