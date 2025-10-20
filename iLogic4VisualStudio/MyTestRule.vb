0c666028cf3857c48c74feb42808123cf8040b82
led-dotcom <74643143+led-dotcom@users.noreply.github.com>
2025-10-20 09:21:43 -0400

    feat: get all parts' names

--- iLogic4VisualStudio/MyTestRule.vb
+++ iLogic4VisualStudio/MyTestRule.vb
@@ -13,38 +13,20 @@ Namespace iLogic4VisualStudio
 
         Public Overrides _
         Sub Main()
-            Dim m_Doc As Inventor.Document = ThisDoc.Document
-            'If m_Doc.DocumentType <> kAssemblyDocumentObject And m_Doc.DocumentType <> kPartDocumentObject Then
-            '    MessageBox.Show("File is not a model.", "iLogic")
-            '    Return 'exit rule
-            'End If
+            ShowReferences()
+        End Sub
 
-            Dim m_Camera As Inventor.Camera = ThisApplication.ActiveView.Camera
-            Dim m_TO As Inventor.TransientObjects = ThisApplication.TransientObjects
+        Public Sub ShowReferences()
+            ' Get the active assembly.
+            Dim oAsmDoc As AssemblyDocument = ThisApplication.ActiveDocument
+            ' Get all of the referenced documents.
+            Dim oRefDocs As DocumentsEnumerator = oAsmDoc.AllReferencedDocuments
 
-            Dim oFileName As String = ThisDoc.FileName(False)
-            'm_Camera.Perspective = True
-
-            m_Camera.ViewOrientationType = Inventor.ViewOrientationTypeEnum.kIsoTopLeftViewOrientation
-            m_Camera.Fit()
-            m_Camera.ApplyWithoutTransition()
-
-            Dim m_CV As Inventor.View = ThisApplication.ActiveView
-
-            m_CV.DisplayMode = Inventor.DisplayModeEnum.kShadedRendering
-            ThisApplication.DisplayOptions.Show3DIndicator = False
-
-            m_CV.Update()
-
-            ' Create name string for the saved image
-            Dim code As String = "WT"
-            Dim length As Int16 = Parameter("Length")
-            Dim width As Int16 = Parameter("Depth")
-            Dim height As Int16 = Parameter("High")
-
-            Dim saveName As String = code & "_" & length & "_" & width & "_" & height
-
-            m_Camera.SaveAsBitmap("C:\Users\di\Desktop\Export\" & saveName & ".png", 1024, 768)
+            ' Iterate through the list of documents.
+            Dim oRefDoc As Document
+            For Each oRefDoc In oRefDocs
+                Logger.Info(oRefDoc.FullFileName)
+            Next
         End Sub
     End Class
 End Namespace
